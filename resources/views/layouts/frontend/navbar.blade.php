6e7e8d49e48dcbbc637de30f45fd193018158401
Muhammad Warisi <mw.a.seven.one@gmail.com>
2025-05-04 15:51:52 +0700

    refactor(homepage): menata tampilan homepage

--- resources/views/layouts/frontend/navbar.blade.php
+++ resources/views/layouts/frontend/navbar.blade.php
@@ -24,18 +24,5 @@
         @foreach ($categories as $category)
         <a class="p-2 text-muted" href="{{$category->slug}}">{{$category->name}}</a>
         @endforeach
-        {{-- <a class="p-2 text-muted" href="#">U.S.</a>
-        <a class="p-2 text-muted" href="#">Technology</a> 
-        <a class="p-2 text-muted" href="#">Design</a>
-        <a class="p-2 text-muted" href="#">Culture</a>
-        <a class="p-2 text-muted" href="#">Business</a>
-        <a class="p-2 text-muted" href="#">Politics</a>
-        <a class="p-2 text-muted" href="#">Opinion</a>
-        <a class="p-2 text-muted" href="#">Science</a>
-        <a class="p-2 text-muted" href="#">Health</a>
-        <a class="p-2 text-muted" href="#">Style</a>
-        <a class="p-2 text-muted" href="#">Travel</a>
-        <a class="p-2 text-muted" href="#">Sakti</a>
-        <a class="p-2 text-muted" href="#">Programming</a> --}}
     </nav>
 </div>
