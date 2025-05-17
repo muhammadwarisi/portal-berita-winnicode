3862b18570fcb923b9650619d62c353597eadf80
Muhammad Warisi <mw.a.seven.one@gmail.com>
2025-05-17 21:37:54 +0700

    refactor(Providers): register reviewServiceInterface & ReviewService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,14 +2,16 @@
 
 namespace App\Providers;
 
-use App\Services\Article\ArticleService;
-use App\Services\Article\ArticleServiceInterface;
 use App\Services\Auth\AuthServices;
-use App\Services\Auth\AuthServiceInterface;
-use App\Services\Homepage\HomepageServiceInterface;
-use App\Services\Homepage\HomepageServices;
+use App\Services\ArticleReview\ReviewService;
 use Illuminate\Support\ServiceProvider;
+use App\Services\Article\ArticleService;
 use RealRashid\SweetAlert\Facades\Alert;
+use App\Services\Auth\AuthServiceInterface;
+use App\Services\Homepage\HomepageServices;
+use App\Services\ArticleReview\ReviewServiceInterface;
+use App\Services\Article\ArticleServiceInterface;
+use App\Services\Homepage\HomepageServiceInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(AuthServiceInterface::class, AuthServices::class);
         $this->app->bind(ArticleServiceInterface::class, ArticleService::class);
         $this->app->bind(HomepageServiceInterface::class, HomepageServices::class);
+        $this->app->bind(ReviewServiceInterface::class, ReviewService::class);
     }
 
     /**
