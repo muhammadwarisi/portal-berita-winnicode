9f9a8287496010603bd7a0f0e619002e7d8674c5
Muhammad Warisi <mw.a.seven.one@gmail.com>
2025-06-02 23:20:52 +0700

    feat(navbar): add conditional dashboard link for authenticated users

--- resources/views/layouts/frontend/navbar.blade.php
+++ resources/views/layouts/frontend/navbar.blade.php
@@ -1,7 +1,7 @@
 <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
         <div class="col-4 pt-1">
-            <a class="blog-header-logo text-dark" href="{{route('homepage')}}">KilasBerita</a>
+            <a class="blog-header-logo text-dark" href="{{ route('homepage') }}">KilasBerita</a>
         </div>
         <div class="col-4 text-center">
             {{-- <a class="text-muted" href="#">Subscribe</a> --}}
@@ -15,7 +15,11 @@
                     <line x1="21" y1="21" x2="15.8" y2="15.8"></line>
                 </svg>
             </a>
-            <a class="btn btn-sm btn-outline-secondary" href="{{ Route('login') }}">Sign up</a>
+            @auth
+                <a class="btn btn-sm btn-outline-secondary" href="{{ Route('dashboard') }}">Dashboard</a>
+            @else
+                <a class="btn btn-sm btn-outline-secondary" href="{{ Route('login') }}">Sign up</a>
+            @endauth
         </div>
     </div>
 </header>
